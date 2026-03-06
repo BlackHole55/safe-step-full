a4fbe20cf701e9e91047f90b08cebf31ff9f31ae
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-03-06 23:16:39 +0500

    fix: add waiting db init and create .env

--- setup.sh
+++ setup.sh
@@ -5,8 +5,17 @@ echo "Starting Safe Step Setup (Linux/Mac)..."
 echo "Building and starting containers..."
 docker-compose up -d --build
 
+docker-compose exec backend cp .env.example .env
+
 echo "Generating App Key..."
 docker-compose exec backend php artisan key:generate
+
+echo "Waiting for Database to be ready..."
+# Loop until 'pg_isready' returns success from inside the db container
+until docker-compose exec db pg_isready -U safestep_user -d safe_step_db; do
+  echo "Database is unavailable - sleeping..."
+  sleep 2
+done
 
 echo "Running Database Migrations and Seeding..."
 docker-compose exec backend php artisan migrate:fresh --seed
